627704e1fbcc6d940aa5db4cc1fdf0ec26a0b361
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-08-31 15:32:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
 }
+
+superbowlWin = (records) => {
+  let result = records.find((record) => record.result === "W");
+  return !!result ? result.year : undefined;
+};
